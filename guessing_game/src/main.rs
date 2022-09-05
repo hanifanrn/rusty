efe6e0aa8705459fed8db5dbc50e34c95ed74800
hanifanrn <hanifanrizki@gmail.com>
2022-09-05 14:43:03 +0700

    update guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     
     let secret_number: u32 = rand::thread_rng().gen_range(1..=100);
     
-    println!("The secret number is: {secret_number}");    
+    println!("The secret number is: {secret_number}");
     
     loop {
         println!("Please input your guess.");
@@ -22,8 +22,9 @@ fn main() {
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
-        };  
-        println!("You guessed: {guess}");   
+        };
+        
+        println!("You guessed: {guess}");
         
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
