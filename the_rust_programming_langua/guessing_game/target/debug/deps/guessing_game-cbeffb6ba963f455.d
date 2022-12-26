cdc468f730dcb945548c1bf8b89efccf6c58cd9a
hanifanrn <hanifanrizki@gmail.com>
2022-12-26 19:03:02 +0700

    clean up

--- the_rust_programming_langua/guessing_game/target/debug/deps/guessing_game-cbeffb6ba963f455.d
+++
@@ -1,5 +0,0 @@
-/Users/hanifannurahman/Documents/1. personal/github/Rusty/guessing_game/target/debug/deps/guessing_game-cbeffb6ba963f455.rmeta: src/main.rs
-
-/Users/hanifannurahman/Documents/1. personal/github/Rusty/guessing_game/target/debug/deps/guessing_game-cbeffb6ba963f455.d: src/main.rs
-
-src/main.rs:
