62262dccdf5bdf9b97b370733d59ea9675cef3e6
hanifanrn <hanifanrizki@gmail.com>
2023-01-17 09:26:58 +0700

    cleaning stuff

--- the_rust_programming_language/target/debug/deps/the_rust_programming_language-2cfc674c3895382c.d
+++
@@ -1,5 +0,0 @@
-/Users/hanifannurahman/Documents/1. personal/github/rusty/the_rust_programming_language/target/debug/deps/the_rust_programming_language-2cfc674c3895382c.rmeta: src/main.rs
-
-/Users/hanifannurahman/Documents/1. personal/github/rusty/the_rust_programming_language/target/debug/deps/the_rust_programming_language-2cfc674c3895382c.d: src/main.rs
-
-src/main.rs:
--- the_rust_programming_language/target/debug/deps/the_rust_programming_language-61fdec4924501c60.d
+++
@@ -1,5 +0,0 @@
-/Users/hanifannurahman/Documents/1. personal/github/rusty/the_rust_programming_language/target/debug/deps/the_rust_programming_language-61fdec4924501c60.rmeta: src/main.rs
-
-/Users/hanifannurahman/Documents/1. personal/github/rusty/the_rust_programming_language/target/debug/deps/the_rust_programming_language-61fdec4924501c60.d: src/main.rs
-
-src/main.rs:
