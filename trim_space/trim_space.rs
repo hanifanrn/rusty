c33cae2b6b17e30f3e882ea88d3ff0b854f9f949
hanifan.nurahman <hanifan.nurahman@ibm.com>
2023-08-10 17:05:18 +0700

    add more file

---
+++ trim_space/trim_space.rs
@@ -0,0 +1,39 @@
+use std::io;
+
+fn main() {
+    let my_string: String = String::from("   my   string   ");
+    println!("{}", trimmed_space(&my_string[..]));
+
+    let my_string_literal: &str = "my  string literal    ";
+    println!("{}", trimmed_space(my_string_literal));
+
+    let mut input_string: String = String::new();
+
+    io::stdin()
+        .read_line(&mut input_string)
+        .expect("Failed to read line");
+
+    println!("{}", trimmed_space(&input_string[..]));
+}
+
+fn trimmed_space(str: &str) -> &str {
+    let chars = str.chars();
+    
+    let mut first_non_space :usize = str.len();
+    let mut last_non_space :usize = 0;
+    
+    for (i, ch) in chars.enumerate() {
+        if ch != ' ' {
+            if first_non_space == str.len() {
+                first_non_space = i;
+            }
+        }
+        last_non_space = i;
+    }
+    
+    if last_non_space == 0 {
+        &str[first_non_space..]
+    } else {
+        &str[first_non_space..last_non_space]
+    }
+}
