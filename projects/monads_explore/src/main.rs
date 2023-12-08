9892de34eb75eb37cd23e42efcb3567a2d2ff531
hanifanrn <hanifanrizki@gmail.com>
2023-12-08 23:01:29 +0700

    add log feature to function

--- projects/monads_explore/src/main.rs
+++ projects/monads_explore/src/main.rs
@@ -1,13 +1,33 @@
-fn square(num: i32) -> i32 {
-    return num * num;
+#[derive(Debug)]
+struct NumWithLogs {
+    num: i32,
+    logs: Vec<Vec<String>>,
 }
 
-fn add_one(num: i32) -> i32 {
-    return num + 1;
+fn square(num: i32) -> NumWithLogs {
+    return NumWithLogs {
+        num: num * num,
+        logs: vec![vec![format!("Squared {} to get {}", num, num * num)]],
+    };
+}
+
+fn add_one(mut num: NumWithLogs) -> NumWithLogs {
+    num.num = num.num + 1;
+    num.logs.push(vec![format!(
+        "Added 1 to {} to get {}",
+        num.num,
+        num.num + 1
+    )]);
+
+    return num;
 }
 
 fn main() {
-    println!("square fn {}, add one fn {}", square(4), add_one(1));
+    println!(
+        "square fn {:#?}, add one fn {:#?}",
+        square(4),
+        add_one(square(2))
+    );
 
-    println!("add one from square {}", add_one(square(4)));
+    println!("add one from square {:#?}", add_one(square(4)));
 }
