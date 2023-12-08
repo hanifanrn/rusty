ed883457ec36816e7dba336c150269e56848810c
hanifanrn <hanifanrizki@gmail.com>
2023-12-09 00:11:24 +0700

    add wrap function to wrap num with logs

--- projects/monads_explore/src/main.rs
+++ projects/monads_explore/src/main.rs
@@ -1,30 +1,48 @@
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 struct NumWithLogs {
     num: i32,
     logs: Vec<String>,
 }
 
-fn square(num: i32) -> NumWithLogs {
-    return NumWithLogs {
-        num: num * num,
-        logs: vec![format!("Squared {} to get {}", num, num * num)],
-    };
+fn wrap_with_logs(num: i32) -> NumWithLogs {
+    return NumWithLogs { num, logs: vec![] };
 }
 
-fn add_one(mut num: NumWithLogs) -> NumWithLogs {
-    num.num = num.num + 1;
-    num.logs
-        .push(format!("Added 1 to {} to get {}", num.num, num.num + 1));
+fn square(num: NumWithLogs) -> NumWithLogs {
+    let mut num_result = num.clone();
+    num_result.num = num.num * num.num;
+    num_result
+        .logs
+        .push(format!("Squared {} to get {}", num.num, num_result.num));
 
-    return num;
+    return num_result;
+}
+
+fn add_one(num: NumWithLogs) -> NumWithLogs {
+    let mut num_result = num.clone();
+    num_result.num = num.num + 1;
+    num_result
+        .logs
+        .push(format!("Added 1 to {} to get {}", num.num, num_result.num));
+
+    return num_result;
 }
 
 fn main() {
     println!(
-        "square fn {:#?}, add one fn {:#?}",
-        square(4),
-        add_one(square(2))
+        "wrap with logs {:#?} square fn {:#?}, add one fn {:#?}",
+        wrap_with_logs(4),
+        square(wrap_with_logs(4)),
+        add_one(square(wrap_with_logs(2)))
     );
 
-    println!("add one from square {:#?}", add_one(square(4)));
+    println!(
+        "add one from square {:#?}",
+        add_one(square(wrap_with_logs(3)))
+    );
+
+    println!(
+        "square a square function {:#?}",
+        square(square(wrap_with_logs(5)))
+    );
 }
