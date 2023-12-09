3c77f8e0349f8228178bb058d1cba974f4e2a63b
hanifanrn <hanifanrizki@gmail.com>
2023-12-09 15:50:59 +0700

    add run_with_logs function so that the flow can be more modular

--- projects/monads_explore/src/main.rs
+++ projects/monads_explore/src/main.rs
@@ -8,41 +8,43 @@ fn wrap_with_logs(num: i32) -> NumWithLogs {
     return NumWithLogs { num, logs: vec![] };
 }
 
-fn square(num: NumWithLogs) -> NumWithLogs {
-    let mut num_result = num.clone();
-    num_result.num = num.num * num.num;
-    num_result
-        .logs
-        .push(format!("Squared {} to get {}", num.num, num_result.num));
+fn run_with_logs(arg: NumWithLogs, func: fn(i32) -> NumWithLogs) -> NumWithLogs {
+    let new_number_with_logs = func(arg.num);
+    let num = new_number_with_logs.num;
+    let mut logs = arg.logs;
+    logs.push(new_number_with_logs.logs[0].clone());
 
-    return num_result;
+    return NumWithLogs { num, logs };
 }
 
-fn add_one(num: NumWithLogs) -> NumWithLogs {
-    let mut num_result = num.clone();
-    num_result.num = num.num + 1;
-    num_result
-        .logs
-        .push(format!("Added 1 to {} to get {}", num.num, num_result.num));
+fn square(num: i32) -> NumWithLogs {
+    let num_result = num * num;
+    let logs = vec![format!("Squared {} to get {}", num, num_result)];
 
-    return num_result;
+    return NumWithLogs {
+        num: num_result,
+        logs,
+    };
+}
+
+fn add_one(num: i32) -> NumWithLogs {
+    let num_result = num + 1;
+    let logs = vec![format!("Added 1 to {} to get {}", num, num_result)];
+
+    return NumWithLogs {
+        num: num_result,
+        logs,
+    };
 }
 
 fn main() {
-    println!(
-        "wrap with logs {:#?} square fn {:#?}, add one fn {:#?}",
-        wrap_with_logs(4),
-        square(wrap_with_logs(4)),
-        add_one(square(wrap_with_logs(2)))
-    );
-
-    println!(
-        "add one from square {:#?}",
-        add_one(square(wrap_with_logs(3)))
-    );
-
-    println!(
-        "square a square function {:#?}",
-        square(square(wrap_with_logs(5)))
-    );
+    let num = 3;
+    let num_a = wrap_with_logs(num);
+    println!("wrap {:#?}", &num_a);
+    let num_b = run_with_logs(num_a, add_one);
+    println!("add 1 {:#?}", &num_b);
+    let num_c = run_with_logs(num_b, square);
+    println!("square from add 1 {:#?}", num_c);
+    let num_d = run_with_logs(num_c, add_one);
+    println!("final result and log {:#?}", num_d);
 }
